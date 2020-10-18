64c4883df6706d94151ddea71196edd971831f21
CodingMantaRay <gabbyculley@gmail.com>
2020-10-18 08:20:03 -0600

    Update script.js
    This update, I changed the locations. Now the prototype has 5 markers around Washakie, Utah, where the Northwestern Band of the Shoshone Nation's land is. We can add more markers from the coronavirus.utah.gov website.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function initMap() {
   mapCenter = tribeLocations[0];
   // Create a new map object
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 3,
+    zoom: 10,
     center: mapCenter,
   });
   // Create an array of alphabetical characters used to label the markers.
@@ -23,37 +23,15 @@ function initMap() {
   
 }
 
-// Add a function to dynamically change the map center
-
-// Change to an array of testing locations
 const testingLocations = [
    // Each location has a latitude and a longitude
-  { lat: -31.56391, lng: 147.154312 },
-  { lat: -33.718234, lng: 150.363181 },
-  { lat: -33.727111, lng: 150.371124 },
-  { lat: -33.848588, lng: 151.209834 },
-  { lat: -33.851702, lng: 151.216968 },
-  { lat: -34.671264, lng: 150.863657 },
-  { lat: -35.304724, lng: 148.662905 },
-  { lat: -36.817685, lng: 175.699196 },
-  { lat: -36.828611, lng: 175.790222 },
-  { lat: -37.75, lng: 145.116667 },
-  { lat: -37.759859, lng: 145.128708 },
-  { lat: -37.765015, lng: 145.133858 },
-  { lat: -37.770104, lng: 145.143299 },
-  { lat: -37.7737, lng: 145.145187 },
-  { lat: -37.774785, lng: 145.137978 },
-  { lat: -37.819616, lng: 144.968119 },
-  { lat: -38.330766, lng: 144.695692 },
-  { lat: -39.927193, lng: 175.053218 },
-  { lat: -41.330162, lng: 174.865694 },
-  { lat: -42.734358, lng: 147.439506 },
-  { lat: -42.734358, lng: 147.501315 },
-  { lat: -42.735258, lng: 147.438 },
-  { lat: -43.999792, lng: 170.463352 },
+  { lat: 41.8193147, lng: -111.831736 }, // Lee's Marketplace Smithfield
+  { lat: 41.8062536, lng: -111.8326689 }, //North Cache Valley Instacare
+  { lat: 41.7749823, lng: -111.8215051 }, //TestUtah Site Logan
+  { lat: 41.72227428544506, lng: -112.18156253055226 }, //Bear River Valley Clinic
+  { lat: 41.7109106, lng: -112.1649271 }, //Ridley's Pharmacy #1151
 ];
 
-// Change to an array of the tribe's locations
 const tribeLocations = [
-	{ lat: -28.024, lng: 140.887 }
+	{ lat: 41.9440909, lng: -112.2171823 } // Washakie, Utah
 ];
