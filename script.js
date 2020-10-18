680610e1f4a8df39d81818c460fdde1d96033591
CodingMantaRay <gabbyculley@gmail.com>
2020-10-17 22:41:04 -0600

    Update script.js
    I realized that we probably can't use the Local Context library because it didn't make it easy to add custom markers. So this just uses the Maps Javascript API.
    
    Demo from here (although I modified it a bit): https://developers.google.com/maps/documentation/javascript/adding-a-google-map

--- script.js
+++ script.js
@@ -1,14 +1,59 @@
-let map;
-
+// This function is called when the webpage first loads
 function initMap() {
-  const localContextMapView = new google.maps.localContext.LocalContextMapView({
-    element: document.getElementById("map"),
-    placeTypePreferences: ["restaurant", "tourist_attraction"],
-    maxPlaceCount: 12,
+  // Create an initial center location
+  mapCenter = tribeLocations[0];
+  // Create a new map object
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 3,
+    center: mapCenter,
   });
-  map = localContextMapView.map;
-  map.setOptions({
-    center: { lat: 51.507307, lng: -0.08114 },
-    zoom: 14,
+  // Create an array of alphabetical characters used to label the markers.
+  const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  // Add some markers to the map.
+  // Note: The code uses the JavaScript Array.prototype.map() method to
+  // create an array of markers based on a given "locations" array.
+  // The map() method here has nothing to do with the Google Maps API.
+  const markers = testingLocations.map((testingLocation, i) => {
+    return new google.maps.Marker({
+      position: testingLocation,
+      label: labels[i % labels.length],
+	  map: map,
+    });
   });
+  
 }
+
+// Add a function to dynamically change the map center
+
+// Change to an array of testing locations
+const testingLocations = [
+   // Each location has a latitude and a longitude
+  { lat: -31.56391, lng: 147.154312 },
+  { lat: -33.718234, lng: 150.363181 },
+  { lat: -33.727111, lng: 150.371124 },
+  { lat: -33.848588, lng: 151.209834 },
+  { lat: -33.851702, lng: 151.216968 },
+  { lat: -34.671264, lng: 150.863657 },
+  { lat: -35.304724, lng: 148.662905 },
+  { lat: -36.817685, lng: 175.699196 },
+  { lat: -36.828611, lng: 175.790222 },
+  { lat: -37.75, lng: 145.116667 },
+  { lat: -37.759859, lng: 145.128708 },
+  { lat: -37.765015, lng: 145.133858 },
+  { lat: -37.770104, lng: 145.143299 },
+  { lat: -37.7737, lng: 145.145187 },
+  { lat: -37.774785, lng: 145.137978 },
+  { lat: -37.819616, lng: 144.968119 },
+  { lat: -38.330766, lng: 144.695692 },
+  { lat: -39.927193, lng: 175.053218 },
+  { lat: -41.330162, lng: 174.865694 },
+  { lat: -42.734358, lng: 147.439506 },
+  { lat: -42.734358, lng: 147.501315 },
+  { lat: -42.735258, lng: 147.438 },
+  { lat: -43.999792, lng: 170.463352 },
+];
+
+// Change to an array of the tribe's locations
+const tribeLocations = [
+	{ lat: -28.024, lng: 140.887 }
+];
